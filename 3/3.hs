edbf1e6c68913195a395c060065a7ff9b0d6506b
Ken Luk <ken.luk@detroitlabs.com>
2016-12-04 16:10:46 -0500

    working, but janky day 3 part 2

--- 3/3.hs
+++ 3/3.hs
@@ -3,11 +3,33 @@ import Data.List
 
 main = do
   input <- readFile "input.txt"
-  putStr $ show $ length $ filter isValidTriangle $ parseLinesToTriangles $ lines input
+  putStr $ show $ validTrianglesByRow $ lines input
+  putStrLn " valid triangles by row"
+  putStrLn "sample rows"
+  putStrLn $ show $ sum $ map colToTriangles $ linesToColumns $ lines input
+
+colToTriangles :: [Int] -> Int
+colToTriangles [] = 0
+colToTriangles xs = if areValidSides
+                       then colToTriangles remaining + 1
+                       else colToTriangles remaining
+  where
+    (sides,remaining) = splitAt 3 xs
+    (a:b:c:_) = sort sides
+    areValidSides = 3 == length sides && c < a + b
+
+linesToColumns :: [String] -> [[Int]]
+linesToColumns lines = transpose $ linesToIntRows lines
+
+linesToIntRows :: [String] -> [[Int]]
+linesToIntRows = foldr (\l acc -> (map strToInt $ words l):acc)  []
+
+validTrianglesByRow :: [String] -> Int
+validTrianglesByRow strs = length $ filter isValidTriangle $ parseLinesToTriangles strs
 
 data Triangle = Triangle { a :: Int
                          , b :: Int
-                         , c :: Int} deriving (Show)
+                         , c :: Int } deriving (Show)
 
 isValidTriangle :: Triangle -> Bool
 isValidTriangle t = c t < a t + b t
@@ -22,9 +44,10 @@ parseLinesToTriangles = foldr f []
 strToTriangle :: String -> Triangle
 strToTriangle str = makeTriangle sortedSides
   where
-    sortedSides = sort $ map (\s -> read s::Int) $ words str
+    sortedSides = sort $ map strToInt $ words str
     makeTriangle (a:b:c:excessSides) = if null excessSides
                                           then Triangle a b c
                                           else error "too many sides!"
     makeTriangle _                   = error "you can only make a triangle with exactly 3 sides"
 
+strToInt s = read s::Int
