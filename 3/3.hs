47f80643536d8193370924b6d148e28074b69eff
Ken Luk <ken.luk@detroitlabs.com>
2016-12-04 19:36:18 -0500

    whatevers:

--- 3/3.hs
+++ 3/3.hs
@@ -1,35 +1,45 @@
 import System.IO
 import Data.List
 
+expectedSideCount = 3
+
 main = do
   input <- readFile "input.txt"
-  let sanitizedRows = linesToIntRows 3 $ lines input
-  putStr $ show $ countValidTriangles $ concat sanitizedRows
+  let sanitizedRows = linesToIntRows expectedSideCount $ lines input
+
+  putStr $ show . length $ filter areValidSides sanitizedRows
   putStrLn " valid triangles by row"
-  putStr $ show $ sum $ map countValidTriangles . (evenMultiples 3) $ transpose sanitizedRows
+  putStr $ show . sum $ map countValidTrianglesSafe $ transpose sanitizedRows
   putStrLn " valid triangles by col"
+
+countValidTrianglesSafe :: [Int] -> Int
+countValidTrianglesSafe sides = countValidTriangles $ truncateExtra expectedSideCount sides
 
 countValidTriangles :: [Int] -> Int
 countValidTriangles [] = 0
-countValidTriangles xs = if areValidSides
+countValidTriangles xs = if areValidSides sides
                             then countValidTriangles remaining + 1
                             else countValidTriangles remaining
   where
-    (sides,remaining) = splitAt 3 xs
-    (a:b:c:_) = sort sides
-    areValidSides = c < a + b
+    (sides,remaining) = splitAt expectedSideCount xs
+
+areValidSides :: [Int] -> Bool
+areValidSides xs = if expectedSideCount == length xs
+                      then let (a:b:c:_) = (sort xs) in c < a + b
+                      else error "Wrong number of sides"
+
 
 linesToIntRows :: Int -> [String] -> [[Int]]
 linesToIntRows rowLength = foldr f []
   where
-    f xs acc = if rowLength == (length $ potentialNums)
+    f xs acc = if rowLength == length potentialNums
                   then (map strToInt $ potentialNums):acc
                   else acc
       where
         potentialNums = words xs
         strToInt = (\x -> read x::Int)
 
-evenMultiples :: Int -> [a] -> [a]
-evenMultiples multLen xs = take numToTake xs
+truncateExtra :: Int -> [a] -> [a]
+truncateExtra multLen xs = take numToTake xs
   where
     numToTake = multLen * (length xs) `div` multLen
