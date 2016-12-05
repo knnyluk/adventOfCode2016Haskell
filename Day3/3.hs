a4e005763f5371f5de62e17e9f2f6842fdef0aa8
Ken Luk <ken.luk@detroitlabs.com>
2016-12-04 21:25:49 -0500

    Adding small optimization for day 3

--- Day3/3.hs
+++ Day3/3.hs
@@ -7,7 +7,7 @@ main = do
   input <- readFile "input.txt"
   let sanitizedRows = linesToIntRows expectedSideCount $ lines input
 
-  putStr $ show . length $ filter areValidSides sanitizedRows
+  putStr $ show $ count areValidSides sanitizedRows
   putStrLn " valid triangles by row"
   putStr $ show . sum $ map countValidTrianglesSafe $ transpose sanitizedRows
   putStrLn " valid triangles by col"
@@ -28,16 +28,20 @@ areValidSides xs = if expectedSideCount == length xs
                       then let (a:b:c:_) = (sort xs) in c < a + b
                       else error "Wrong number of sides"
 
-
 linesToIntRows :: Int -> [String] -> [[Int]]
 linesToIntRows rowLength = foldr f []
   where
     f xs acc = if rowLength == length potentialNums
-                  then (map strToInt $ potentialNums):acc
+                  then (map strToInt potentialNums):acc
                   else acc
       where
         potentialNums = words xs
         strToInt = (\x -> read x::Int)
+
+count :: (a -> Bool) -> [a] -> Int
+count f = foldr g 0
+  where
+    g x acc = if f x then acc + 1 else acc
 
 truncateExtra :: Int -> [a] -> [a]
 truncateExtra multLen xs = take numToTake xs
